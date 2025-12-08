07e760589efd03dbfe779c6e10235ccfb2f20a77
Petar Cevriz <pcevriz95@gmail.com>
2025-12-08 15:40:09 +0100

    Add an initial idea for the day2 solution, it is wrong so look into the solution

---
+++ day2/main.py
@@ -0,0 +1,32 @@
+import re
+
+
+def find_invalid_ids(start, end):
+    invalid_ids = set()
+    for id in range(start, end + 1):
+        test_id = str(id)
+        invalid_id = re.search(r"^(\d+)\1+$", test_id)
+        if invalid_id:
+            invalid_ids.add(id)
+
+    return invalid_ids
+
+
+def main():
+    pass
+    invalid_ids = []
+    # Load the id range data
+    with open("input.txt", "r") as input_file:
+        ranges = input_file.read().split(",")
+    # Get the list of invalid ids from the range
+    for rng in ranges:
+        start, end = map(int, rng.split("-"))
+        invalid_ids.extend(find_invalid_ids(start, end))
+    invalid_ids = set(invalid_ids)
+    result = sum(invalid_ids)
+    print(result)
+
+
+# TODO - I am getting a number that is too high, check what the issue is
+if __name__ == "__main__":
+    main()
