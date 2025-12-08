6ffcc4d0f4069eefbf5e8d75768c98427d128455
Petar Cevriz <pcevriz95@gmail.com>
2025-12-08 16:31:24 +0100

    This approach seems like it is wrong also, will need to keep looking

--- day2/main.py
+++ day2/main.py
@@ -5,20 +5,17 @@ def find_invalid_ids(start, end):
     invalid_ids = set()
     for id in range(start, end + 1):
         test_id = str(id)
-        invalid_id = re.search(r"^(\d+)\1+$", test_id)
+        invalid_id = re.search(r"^(\d+?)\1+$", test_id)
         if invalid_id:
-            invalid_ids.add(id)
+            invalid_ids.add(int(invalid_id.group(1)))
 
     return invalid_ids
 
 
 def main():
-    pass
     invalid_ids = []
-    # Load the id range data
     with open("input.txt", "r") as input_file:
         ranges = input_file.read().split(",")
-    # Get the list of invalid ids from the range
     for rng in ranges:
         start, end = map(int, rng.split("-"))
         invalid_ids.extend(find_invalid_ids(start, end))
